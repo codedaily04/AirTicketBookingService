d711eb8a2aebd62b22364ec14633effebe40454a
Abhishek Nishad <codedaily04@gmail.com>
2026-04-19 16:32:49 +0530

    added some testing units in routes for api gateway

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,7 +8,11 @@ const { BookingController }=require('../../controllers/index');
 const bookingController=new BookingController();
 const router=express.Router();
 
-
+router.get('/info',(req,res)=>{
+    return res.json({
+        message: 'This is the booking service'
+    });
+});
 router.post('/bookings',bookingController.create);
 router.post('/publish', bookingController.sendMessageToQueue);
 
